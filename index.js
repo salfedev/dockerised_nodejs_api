91ba805a2244da8c8f6898147242f9fdb5b17c07
salfsdev <sal.abuewilly@gmail.com>
2022-09-25 05:00:03 +0100

    Add .dockerignore file

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send("<h3>It's Alive!!");
+	res.send("<h3>It's alive!!!</h3>");
+});
+
+app.get('/version', (req, res) => {
+	res.send({api_version: '0.0.0', client_version: 0});
 });
 
 app.listen(port, () => {
