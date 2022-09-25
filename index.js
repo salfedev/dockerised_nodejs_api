e6403d3aaeea3248713b3db28b2857e8c58236a3
salfsdev <sal.abuewilly@gmail.com>
2022-09-25 05:05:01 +0100

    Health and Version routes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/health', (_req, res) => {
 	res.send("<h3>It's alive!!!</h3>");
 });
 
-app.get('/version', (req, res) => {
+app.get('/version', (_req, res) => {
 	res.send({api_version: '0.0.0', client_version: 0});
 });
 
